e9d3e0ad5a1d52c0277674bc7c95137c493a2c39
David Hodgkinson <lelunedor@gmail.com>
2013-05-14 23:28:04 +0100

    Initial Commit

---
+++ openGLWrapperTest/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  openGLWrapperTest
+//
+//  Created by David Hodgkinson on 14/05/2013.
+//  Copyright (c) 2013 David Hodgkinson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ openGLWrapperTestTests/openGLWrapperTestTests.h
@@ -0,0 +1,13 @@
+//
+//  openGLWrapperTestTests.h
+//  openGLWrapperTestTests
+//
+//  Created by David Hodgkinson on 14/05/2013.
+//  Copyright (c) 2013 David Hodgkinson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface openGLWrapperTestTests : SenTestCase
+
+@end
